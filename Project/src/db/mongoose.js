084927f5d7a4a26fdf190c69c63efb37ddf0c218
Akash-sahu-p <108019992+Akash-sahu-p@users.noreply.github.com>
2024-06-17 00:13:07 +0530

    Update mongoose.js
    a typo fix

--- Project/src/db/mongoose.js
+++ Project/src/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose') //iske lie npm i mongoose karna hhh
 
-cons uri = 'mongodb+srv://akashsahu:akashsahu@node-tuts-cluster.rzcder3.mongodb.net/?retryWrites=true&w=majority&appName=node-tuts-cluster'
+const uri = 'mongodb+srv://akashsahu:akashsahu@node-tuts-cluster.rzcder3.mongodb.net/?retryWrites=true&w=majority&appName=node-tuts-cluster'
 mongoose.connect(uri,{
     ///userNewUrlParser: true,
     //userCreateIndex: true
